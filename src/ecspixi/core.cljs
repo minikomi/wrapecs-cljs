dfa2e90d70c76a54976ddbdb0ce6baa4158b5dcf
Adam Moore <nerdfunk@gmail.com>
2017-06-19 11:50:16 +0900

    kw accessors for commonly used vars on components

--- src/ecspixi/core.cljs
+++ src/ecspixi/core.cljs
@@ -6,7 +6,13 @@
 
 (enable-console-print!)
 
+;; constants
+
 (def P js/PIXI)
+(def W (.. js/window -document -body -clientWidth))
+(def H (.. js/window -document -body -clientHeight))
+
+;; util
 
 (defn vec->str-arr [^:not-native coll]
   (let [arr (array)]
@@ -14,25 +20,19 @@
       (.push arr (name v)))
     arr))
 
-(def W (.. js/window -document -body -clientWidth))
-(def H (.. js/window -document -body -clientHeight))
-
-(def bunny-texture (.fromImage P.Texture "bunnys.png"))
-(def textures
-  (mapv (fn [y]
-            (P.Texture.
-              (.-baseTexture bunny-texture)
-              (P.math.Rectangle. 2 y 26 37)))
-        [47 86 125 164 2]))
-
-(defn get-sprite []
-  (P.Sprite. (rand-nth textures)))
-
 (defn get-component [entity c-name]
   (.get entity (name c-name)))
 
-(defn component-set [e c-name ks v]
-  (oset!+ (get-component e c-name) ks v))
+(defn add-component [entity c-name c-data]
+  (.addComponent entity (name c-name) c-data))
+
+(defn query-components [em cs]
+  (.queryComponents em (vec->str-arr cs)))
+
+(defn clamp [v l h]
+  (min h (max l v)))
+
+;; components
 
 (defprotocol IVelocity
   (velocity-set [v dx' dy'])
@@ -41,6 +41,12 @@
   (gravity [_]))
 
 (deftype Velocity [^:mutable dx ^:mutable dy]
+  IFn
+  (-invoke [this kw]
+    (case kw
+      :dx dx
+      :dy dy
+      nil))
   IVelocity
   (velocity-set [v dx' dy']
     (set! dx dx')
@@ -50,18 +56,32 @@
   (gravity [_] (set! dy (inc dy))))
 
 (defprotocol IDrawable
-  (pos-x [_])
-  (pos-y [_])
-  (pos-set [_ x y]))
+  (position-set [_ x y]))
 
 (deftype Drawable [^:mutable sprite]
+  IFn
+  (-invoke [this kw]
+    (case kw
+      :x (.-x (.-position sprite))
+      :y (.-y (.-position sprite))
+      nil))
   IDrawable
-  (pos-x [_] (.. sprite -position -x))
-  (pos-y [_] (.. sprite -position -y))
-  (pos-set [_ x y] (.set (.-position sprite) x y)))
+  (position-set [_ x y]
+    (.set (.-position sprite) x y)))
 
-(defn add-component [entity c-name c-data]
-  (.addComponent entity (name c-name) c-data))
+;; sprite
+
+(def bunny-texture (.fromImage P.Texture "bunnys.png"))
+
+(def textures
+  (mapv (fn [y]
+          (P.Texture.
+           (.-baseTexture bunny-texture)
+           (P.Rectangle. 2 y 26 37)))
+        [47 86 125 164 2]))
+
+(defn get-sprite []
+  (P.Sprite. (rand-nth textures)))
 
 (defn make-bunny [em stage x y]
   (let [bunny (.createEntity em)
@@ -77,19 +97,12 @@
                    (Velocity. (- (rand-int 10) 5)
                               (- (rand-int 10) 5)))))
 
-(defn query-components [em cs]
-  (.queryComponents em (vec->str-arr cs)))
-
-(defn rev-x [v]
-  (oset! v :dx (- (oget v :dx))))
-
-(defn rev-y [v]
-  (oset! v :dy (- (oget v :dy))))
+;; systems
 
 (defn bounce-update [em]
   (doseq [e (query-components em [:drawable :velocity])]
-    (let [x (pos-x (get-component e :drawable))
-          y (pos-y (get-component e :drawable))
+    (let [x ((get-component e :drawable) :x)
+          y ((get-component e :drawable) :y)
           vel (get-component e :velocity)]
       (when (or (>= 0 x) (<= W x))
         (bounce-x vel))
@@ -97,15 +110,15 @@
         (bounce-y vel)
         (gravity vel)))))
 
-(defn clamp [v l h]
-  (min h (max l v)))
-
 (defn move-update [em]
   (doseq [e (query-components em [:drawable :velocity])]
     (let [drw (get-component e :drawable)
           vel (get-component e :velocity)]
-      (pos-set drw (+ (pos-x drw) (.-dx vel))
-                   (+ (pos-y drw) (.-dy vel))))))
+      (position-set drw
+                    (+ (drw :x) (vel :dx))
+                    (+ (drw :y) (vel :dy))))))
+
+;; main loop
 
 (defn game []
   (let [dom-node (atom false)
